173ea61ab63cec4c88ebaadf8f7b6a06c7e9c445
antonivels <75495337+antonivels@users.noreply.github.com>
2023-03-31 19:37:40 +0300

    4 files modified

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import { useState } from 'react';
 
 export default function Home() {
+  const [output, setOutput] = useState([]);
   return (
     <div className={styles.container}>
       <Head>
@@ -9,51 +11,144 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <h1 className={styles.title}>calc</h1>
       </main>
+      <div className="output">{output}</div>
+      <div>
+        {/*///////////////////////////////////////////////////////////
+        ///////////////////////// noumera////////////////////////// */}
+        <div className="row">
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '1');
+            }}
+          >
+            1
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '2');
+            }}
+          >
+            2
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '3');
+            }}
+          >
+            3
+          </button>
+        </div>
 
-      <footer className={styles.footer}>
-        <a href="https://next.new" target="_blank" rel="noopener noreferrer">
-          Created with&nbsp;<b>next.new</b>&nbsp;⚡️
-        </a>
-      </footer>
+        <div className="row">
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '4');
+            }}
+          >
+            4
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '5');
+            }}
+          >
+            5
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '6');
+            }}
+          >
+            6
+          </button>
+        </div>
+
+        <div className="row4">
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '7');
+            }}
+          >
+            7
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '8');
+            }}
+          >
+            8
+          </button>
+          <button
+            className="button"
+            onClick={() => {
+              setOutput(output + '9');
+            }}
+          >
+            9
+          </button>
+
+          <button
+            className="button"
+            onClick={() => setOutput(output.substr(0, output.length - 1))}
+          >
+            Backspace
+          </button>
+        </div>
+
+        {/*///////////////////////////////////////////////////////////
+        ///////////////////////// noumera////////////////////////// */}
+
+        <div className="row4">
+          <button className="button" onClick={() => setOutput(output + '+')}>
+            +
+          </button>
+          <button className="button" onClick={() => setOutput(output + '-')}>
+            -
+          </button>
+          <button className="button" onClick={() => setOutput(output + '*')}>
+            *
+          </button>
+          <button className="button" onClick={() => setOutput(output + '.')}>
+            .
+          </button>
+        </div>
+        <div className="row4">
+          <button className="button" onClick={() => setOutput(output + '/')}>
+            /
+          </button>
+          <button
+            className="equalbutton"
+            onClick={() => setOutput(eval(output))}
+          >
+            =
+          </button>
+          <button className="button" onClick={() => setOutput(' ')}>
+            C
+          </button>
+        </div>
+      </div>
     </div>
   );
+}
+
+// function equalbuttonsolve() {
+//   setOutput(eval(output));
+// }
+
+// function equalbuttonmake() {
+//   setOutput(output);
+// }
+
+if() {
+
 }
